14abc4af0eab7bd739e4434d4a9837452afc4b77
Avi Fisher <avifisher82@gmail.com>
2025-12-10 12:53:01 +0200

    fix "editPrice" function

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ export function editPrice(id, newPrice) {
     if (find == undefined) {
         console.log("The product not found");
     } else {
-        find = newPrice
+        find.price = newPrice
         console.log("Save new price");
     }
 }
