bfa0f0a2ed4ec922846c61fe8ba626c0728d9456
hagay kohelet <hagay2506@gmail.com>
2025-12-10 12:29:55 +0200

    cli file

---
+++ cli.js
@@ -0,0 +1,50 @@
+import input from "./uttils.js";
+
+
+export default function printCli() {
+    console.log(`    === marketplace manager ===
+        1. append item to sell
+        2. edit an exist product
+        3. show all items
+        4. show information on items
+        5. delete an item 
+        6. filter on about category
+        0. exit\n`);
+}
+let flag = true
+while (flag) {
+    printCli()
+    let choice = input("please enter your choice from menu: ")
+    
+    switch (choice) {
+        case "1":
+            let title = input("\nenter a title of your items: ")
+            let price = input("enter a price of your product: ")
+            let description = input("enter a description of your product: ")
+            let category = input("enter a category of this item: ")
+            addProduct(title, price, description, category)
+
+        case "2":
+            let findId = input("\nplease enter product id: ")
+            let newPrice = input("please enter a new price: ")
+            editPrice(findId, newPrice)
+        
+        case "3":
+            showByPriceOrder(DB)
+
+        case "4":
+            let itemId = input("\nplease enter your item id: ")
+            showByPriceDetails(itemId)
+        
+        case "5":
+            let id = input("\nplease enter item id do you want to delete: ")
+            removeProduct(id)
+        
+        case "0":
+            flag = false
+            break
+        default: 
+            console.log("\ninvalid input please enter from menu!\n ");
+        
+    }
+}
